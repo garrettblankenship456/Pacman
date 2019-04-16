e3c750634ca5ada38dd488f7ccd5a75a7d7d2dc5
GARRETT WILLIAM BLANKENSHIP <1131323@psd202.org>
2019-04-16 09:24:17 -0500

    Created symmetry

--- world.py
+++ world.py
@@ -9,6 +9,7 @@ class World:
         self.worldData = []
         self.worldPolys = []
         self.mirrored = False
+        self.lineOfSymmetry = 0
         
         # generate world data
         self.__genWorldData("levels/default.txt")
@@ -20,42 +21,48 @@ class World:
             # Get mirror status
             if line == "mirror_true\n":
                 self.mirrored = True
-                continue
+            elif line.split(" ")[0] == "center_line":
+                self.lineOfSymmetry = int(line.split(" ")[1]) * config.MAP_RESOLUTION_X
+            else:
+                # Get position and size points
+                positions = []
+                positions2 = []
+                points = line.split(" ")
 
-            # Get position and size points
-            positions = []
-            points = line.split(" ")
+                # Get XY from points
+                pointData = points[0].split(",")
+                xPos = int(pointData[0]) * config.MAP_RESOLUTION_X
+                yPos = int(pointData[1]) * config.MAP_RESOLUTION_Y
 
-            # Get XY from points
-            pointData = points[0].split(",")
-            xPos = int(pointData[0]) * config.MAP_RESOLUTION_X
-            yPos = int(pointData[1]) * config.MAP_RESOLUTION_Y
+                # Get size from points
+                pointData = points[1].split(",")
+                xSize = int(pointData[0]) * config.MAP_RESOLUTION_X
+                ySize = int(pointData[1]) * config.MAP_RESOLUTION_Y
 
-            # Get size from points
-            pointData = points[1].split(",")
-            xSize = int(pointData[0]) * config.MAP_RESOLUTION_X
-            ySize = int(pointData[1]) * config.MAP_RESOLUTION_Y
+                # Create positions from pos and size
+                positions.append(Point(xPos, yPos))
+                positions.append(Point(xPos + xSize, yPos))
+                positions.append(Point(xPos + xSize, yPos + ySize))
+                positions.append(Point(xPos, yPos + ySize))
 
-            # Create positions from pos and size
-            positions.append(Point(xPos, yPos))
-            positions.append(Point(xPos + xSize, yPos))
-            positions.append(Point(xPos + xSize, yPos + ySize))
-            positions.append(Point(xPos, yPos + ySize))
+                # Put array in world data
+                self.worldData.append(positions)
 
-            # Put array in world data
-            self.worldData.append(positions)
+                # If mirrored append a backwards array of positions
+                if self.mirrored == True:
+                    positions2.append(Point(self.lineOfSymmetry * 2 - xPos, yPos))
+                    positions2.append(Point(self.lineOfSymmetry * 2 - xPos - xSize, yPos))
+                    positions2.append(Point(self.lineOfSymmetry * 2 - xPos - xSize, yPos + ySize))
+                    positions2.append(Point(self.lineOfSymmetry * 2 - xPos, yPos + ySize))
+                    self.worldData.append(positions2)
+                    poly2 = Polygon(positions2)
+                    poly2.setFill("black")
+                    self.worldPolys.append(poly2)
 
-            # If mirrored append a backwards array of positions
-            positions.append(Point(-xPos, yPos))
-            positions.append(Point(-xPos - xSize, yPos))
-            positions.append(Point(-xPos - xSize, yPos + ySize))
-            positions.append(Point(-xPos, yPos + ySize))
-            self.worldData.append(positions)
-
-            # Loop through each line and make a polygon from it
-            poly = Polygon(positions)
-            poly.setFill("black")
-            self.worldPolys.append(poly)
+                # Loop through each line and make a polygon from it
+                poly = Polygon(positions)
+                poly.setFill("black")
+                self.worldPolys.append(poly)
 
     def render(self, window):
         """Draws world to the GraphWin given"""
